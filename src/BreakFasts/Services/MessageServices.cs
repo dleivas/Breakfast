fa85ef9b811e5312d0e0364b8265d4f5a46394bb
Diego Leivas <diego@gmail.com>
2015-11-10 19:00:56 -0500

    1

---
+++ src/BreakFasts/Services/MessageServices.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Framework.OptionsModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BreakFasts.Services
+{
+    // This class is used by the application to send Email and SMS
+    // when you turn on two-factor authentication in ASP.NET Identity.
+    // For more details see this link http://go.microsoft.com/fwlink/?LinkID=532713
+    public class AuthMessageSender : IEmailSender, ISmsSender
+    {
+        public AuthMessageSender(IOptions<AuthMessageSenderOptions> optionsAccessor)
+        {
+            Options = optionsAccessor.Value;
+        }
+
+        public AuthMessageSenderOptions Options { get; }  // set only via Secret Manager
+
+
+        public Task SendEmailAsync(string email, string subject, string message)
+       {
+        // Plug in your email service here to send an email.
+        var myMessage = new SendGrid.SendGridMessage();
+        myMessage.AddTo(email);
+        myMessage.From = new System.Net.Mail.MailAddress("dleivas@gmail.com", "Diegui Leivas");
+        myMessage.Subject = subject;
+        myMessage.Text = message;
+        myMessage.Html = message;
+        var credentials = new System.Net.NetworkCredential(
+            Options.SendGridUser,
+            Options.SendGridKey);
+        // Create a Web transport for sending email.
+        var transportWeb = new SendGrid.Web(credentials);
+        // Send the email.
+        if (transportWeb != null)
+        {
+            return transportWeb.DeliverAsync(myMessage);
+        }
+        else
+        {
+            return Task.FromResult(0);
+        }
+        }
+        
+        public Task SendSmsAsync(string number, string message)
+        {
+            var twilio = new Twilio.TwilioRestClient(
+                Options.SID,           // Account Sid from dashboard
+                Options.AuthToken);    // Auth Token
+
+            var result = twilio.SendMessage(Options.SendNumber, number, message);
+            // Use the debug output for testing without receiving a SMS message.
+            // Remove the Debug.WriteLine(message) line after debugging.
+            // System.Diagnostics.Debug.WriteLine(message);
+            return Task.FromResult(0);
+        }
+    }
+}
